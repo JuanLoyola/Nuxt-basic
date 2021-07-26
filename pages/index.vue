cd4894a959cbd24b4e252937ff6f28d30357c15d
Loyola <juanloyola@hotmail.com>
2021-07-26 15:41:26 -0300

    Nuxt basics

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Nav/>
+    <div class="container">
+      <Hero/>
+      <LargeCardDisplay
+        v-for="cardInfo in largeCardInfo"
+        :key="cardInfo.id"
+        :cardSection="cardInfo"
+      />
+      <SmallCardDisplay
+        v-for="cardInfo in smallCardSections"
+        :key="cardInfo.id"
+        :cardSection="cardInfo"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { largeCardSections, smallCardSections } from '@/assets/data.js'
+export default {
+  data() {
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardSections
+    }
+  }
+}
+</script>
+
+<style>
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+</style>
---
+++ pages/my-items.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    my items page
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style scoped>
+
+</style>
